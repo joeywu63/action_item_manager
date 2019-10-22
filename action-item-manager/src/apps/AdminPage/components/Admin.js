8368abaf78f4e86fa46fe37bee22ce2e2bc8d836
unknown <joeywu634@gmail.com>
2019-10-21 21:25:33 -0400

    modified files

--- action-item-manager/src/apps/AdminPage/components/Admin.js
+++ action-item-manager/src/apps/AdminPage/components/Admin.js
@@ -4,11 +4,7 @@ import styled from 'styled-components';
 import Button from 'common/Button';
 import Header from 'common/Header';
 
-<<<<<<< HEAD
 import TeamTable from './TeamTable';
-=======
-import UserTable from './TeamTable';
->>>>>>> 0b570c8ab9b323d4e576642854ab2888437c47e1
 
 import { getAllTeams, addTeamToList, removeTeamFromList } from 'model/team'
 
@@ -22,11 +18,7 @@ class Admin extends React.Component {
         return (
             <>
                 <Header title="Welcome, Admin"/>
-<<<<<<< HEAD
                 <TeamTable teams={this.state.teams}
-=======
-                <UserTable teams={this.state.teams}
->>>>>>> 0b570c8ab9b323d4e576642854ab2888437c47e1
                            removeTeam={removeTeamFromList}
                            addTeam={addTeamToList}/>
             </>
