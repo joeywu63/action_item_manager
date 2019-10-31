e3fb3aa01a462ce06e423f228192df568aa6f2f8
unknown <joeywu634@gmail.com>
2019-10-31 00:13:28 -0400

    managers can edit action items

--- action-item-manager/src/apps/ActionItems/repository.js
+++ action-item-manager/src/apps/ActionItems/repository.js
@@ -19,6 +19,22 @@ export const updateDescription = ({ id, newDesc }) => {
     return actionItem.updateDescription({ id, newDesc });
 };
 
+export const getTeamByID = ({ teamId }) => {
+    return team.getByID({ teamId });
+};
+
+export const getActionItemByID = ({ actionItemId }) => {
+    return actionItem.getByID({ id: actionItemId });
+};
+
+export const updateTitle = ({ id, newTitle }) => {
+    return actionItem.updateTitle({ id, newTitle });
+};
+
+export const updateDescription = ({ id, newDesc }) => {
+    return actionItem.updateDescription({ id, newDesc });
+};
+
 export const toggleActionItemComplete = actionItem.toggleActionItemComplete;
 
 export const didComplete = actionItem.didComplete;
