f185d8c4f151d227c2a310070a5a2bf24bd67056
unknown <joeywu634@gmail.com>
2019-10-31 01:12:20 -0400

    rebasing

--- action-item-manager/src/apps/ActionItems/repository.js
+++ action-item-manager/src/apps/ActionItems/repository.js
@@ -19,22 +19,6 @@ export const updateDescription = ({ id, newDesc }) => {
     return actionItem.updateDescription({ id, newDesc });
 };
 
-export const getTeamByID = ({ teamId }) => {
-    return team.getByID({ teamId });
-};
-
-export const getActionItemByID = ({ actionItemId }) => {
-    return actionItem.getByID({ id: actionItemId });
-};
-
-export const updateTitle = ({ id, newTitle }) => {
-    return actionItem.updateTitle({ id, newTitle });
-};
-
-export const updateDescription = ({ id, newDesc }) => {
-    return actionItem.updateDescription({ id, newDesc });
-};
-
 export const toggleActionItemComplete = actionItem.toggleActionItemComplete;
 
 export const didComplete = actionItem.didComplete;
