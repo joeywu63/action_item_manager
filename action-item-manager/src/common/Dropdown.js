a18f5ef11bb2b666123282783d66f085d930996b
john doe <johndoe@johns-MacBook-Pro.local>
2019-11-01 21:35:48 -0400

    first pass at styling dropdown

--- action-item-manager/src/common/Dropdown.js
+++ action-item-manager/src/common/Dropdown.js
@@ -5,12 +5,26 @@ class Dropdown extends React.Component {
     render() {
         const { placeholder, value, onChange, options } = this.props;
 
+        const customStyles = {
+            control: styles => ({...styles, height: 35, minHeight: 35, width: 400 }),
+            option: styles => ({
+                ...styles,
+                width: 400,
+                height: 35
+            }),
+            container: styles => ({
+                ...styles,
+                width: 400
+            })
+        };
+
         return (
             <Select
                 placeholder={placeholder}
                 value={value}
                 onChange={onChange}
                 options={options}
+                styles={customStyles}
             />
         );
     }
