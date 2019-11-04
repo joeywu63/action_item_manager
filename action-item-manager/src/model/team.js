328fe37f6ee0c6756990bcbeafbdb1aa2ef56b53
unknown <joeywu634@gmail.com>
2019-11-03 20:33:45 -0500

    fixed user not being added to created team

--- action-item-manager/src/model/team.js
+++ action-item-manager/src/model/team.js
@@ -17,8 +17,7 @@ const teams = [
     }
 ];
 
-
-// server/database get call 
+// server/database get call
 export const getByID = ({ teamId }) => {
     const team = teams.filter(t => t.id === teamId);
     try {
@@ -28,7 +27,7 @@ export const getByID = ({ teamId }) => {
     }
 };
 
-// server/database get call 
+// server/database get call
 export const getSize = ({ teamId }) => {
     let result = 0;
     const users = getList();
@@ -42,14 +41,14 @@ export const getSize = ({ teamId }) => {
     return result;
 };
 
-// server/database get call 
+// server/database get call
 export const getTeamsByCurrentUser = () => {
     const currentUser = getCurrentUser();
 
     return teams.filter(team => currentUser.teamIDList.includes(team.id));
 };
 
-// server/database get call 
+// server/database get call
 export const getUsers = ({ teamID }) => {
     // return two arrays. One of users on the team specified, and one of users not on the team specified
     const users = getList();
@@ -60,7 +59,7 @@ export const getUsers = ({ teamID }) => {
     return { onTeam, offTeam };
 };
 
-// server/database update call 
+// server/database update call
 export const setManager = ({ teamId, managerId }) => {
     const team = teams.find(team => team.id === teamId);
     try {
@@ -73,12 +72,12 @@ export const setManager = ({ teamId, managerId }) => {
     }
 };
 
-// server/database get call 
+// server/database get call
 export const getAllTeams = () => {
     return teams;
 };
 
-// server/database push call 
+// server/database push call
 export const addTeamToList = ({ teamName, managerID }) => {
     const newTeam = {
         id: numTeams + 1,
@@ -86,11 +85,12 @@ export const addTeamToList = ({ teamName, managerID }) => {
         managerID: managerID
     };
     teams.push(newTeam);
+    addToTeam({ userID: managerID, teamID: newTeam.id });
     numTeams++;
     return newTeam;
 };
 
-// server/database delete call 
+// server/database delete call
 export const removeTeamFromList = ({ teamId }) => {
     return teams.forEach((team, i) => {
         if (team.id === teamId) {
