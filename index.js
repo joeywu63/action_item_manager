a8df29f38a51f8469e50ced9c372bbf1d2253e49
john doe <johndoe@johns-MacBook-Pro.local>
2019-11-17 23:09:47 -0500

    testing

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.use(express.static(__dirname + '/client/build'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 app.get('/test', (req, res) => {
     res.send('test');
