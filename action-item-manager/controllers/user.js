a802cc478787be1e7e6a09f41947f29583461e0b
john doe <johndoe@johns-MacBook-Pro.local>
2019-11-17 22:09:32 -0500

    adding heroku controller

---
+++ action-item-manager/controllers/user.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname + '/pub'));
+
+app.get('/', (req, res) => {
+    res.send('test');
+});
+
+// Error codes
+// app.get('/problem', (req, res) => {
+    // You can indicate a status code to send back
+    // by default it is 200, but it's up to you
+    // if you want to send something
+    // res.status(500).send('There was a problem on the server');
+
+    // don't send nonsense status codes like this one:
+    //res.status(867).send('There was a problem on the server')
+// });
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    log(`Listening on port ${port}...`);
+}); // localhost development port 3001  (http://localhost:3001)
