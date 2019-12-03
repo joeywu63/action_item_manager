f8e3687af5d1f41fa3fb616f1ed1d69fb80a9956
Sandro <sandroseryani@yahoo.com>
2019-12-02 23:32:29 -0500

    Added dividing teams functionality

--- action-item-manager/src/apps/Teams/components/TeamList.js
+++ action-item-manager/src/apps/Teams/components/TeamList.js
@@ -5,35 +5,90 @@ import TeamPanel from './TeamPanel';
 
 import Header from 'common/Header';
 import LoadingIndicator from 'common/LoadingIndicator';
+import { getCurrentUser } from 'utils/currentUser';
 
 import { getTeams } from '../repository';
 
 const TeamsWrapper = styled.div`
     display: flex;
     flex-direction: row;
+    padding-bottom: 70px;
+    border-bottom : ${props => props.last ? 'none' : '1px solid black'}; 
 `;
 
 class TeamList extends React.Component {
     async componentDidMount() {
+        const { managedTeams, participantTeams } = this.state;
         const teams = await getTeams();
-        this.setState({ loading: false, teams });
+        const id = getCurrentUser()._id;
+        teams.forEach(team => {
+            if (team.managerID === id) {
+                managedTeams.push(team);
+            } else {
+                participantTeams.push(team);
+            }
+        });
+        this.setState({
+            loading: false,
+            teams,
+            managedTeams,
+            participantTeams
+        });
     }
 
     state = {
         loading: true,
-        teams: []
+        managedTeams: [],
+        participantTeams: []
     };
 
     renderTeams = () => {
-        const { teams } = this.state;
+        const { managedTeams, participantTeams } = this.state;
+        const managingTitle = 'Teams Managing';
+        const participatingTitle = 'Teams Participating';
 
-        return (
-            <TeamsWrapper>
-                {teams.map(team => (
-                    <TeamPanel key={team._id} team={team} />
-                ))}
-            </TeamsWrapper>
-        );
+        if (managedTeams.length === 0 && participantTeams.length === 0) {
+            return <h3>You have no teams</h3>
+        } else if (managedTeams.length !== 0 && participantTeams.length === 0) {
+            return (
+                <>
+                    <h2>{managingTitle}</h2>
+                    <TeamsWrapper last={true}>
+                        {managedTeams.map(team => (
+                            <TeamPanel key={team._id} team={team} />
+                        ))}
+                    </TeamsWrapper>
+                </>
+            );
+        } else if (managedTeams.length === 0 && participantTeams.length !== 0) {
+            return (
+                <>
+                    <h2>{participatingTitle}</h2>
+                    <TeamsWrapper last={true}>
+                        {participantTeams.map(team => (
+                            <TeamPanel key={team._id} team={team} />
+                        ))}
+                    </TeamsWrapper>
+                </>
+            );
+        } else {
+            return (
+                <>
+                    <h2>{managingTitle}</h2>
+                    <TeamsWrapper last={false}>
+                        {managedTeams.map(team => (
+                            <TeamPanel key={team._id} team={team} />
+                        ))}
+                    </TeamsWrapper>
+                    <h2>{participatingTitle}</h2>
+                    <TeamsWrapper last={true}>
+                        {participantTeams.map(team => (
+                            <TeamPanel key={team._id} team={team} />
+                        ))}
+                    </TeamsWrapper>
+                </>
+            );
+        }
     };
 
     render() {
